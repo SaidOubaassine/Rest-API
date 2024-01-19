c6ba83af49871f185dd4374813566f24ba8475f2
SaidOubaassine <saidoubaassine04@gmail.com>
2024-01-19 16:06:07 +0100

    read one author

--- api/src/main/java/learn/spring/api/services/AuthorService.java
+++ api/src/main/java/learn/spring/api/services/AuthorService.java
@@ -3,9 +3,12 @@ package learn.spring.api.services;
 import learn.spring.api.domain.entities.AuthorEntity;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AuthorService {
     AuthorEntity save(AuthorEntity authorEntity);
 
     List<AuthorEntity> findAll();
+
+    Optional<AuthorEntity> findOne(Long id);
 }
