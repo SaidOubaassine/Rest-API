2082fa2e0892f68a612fb3b02b31f4845de9b082
SaidOubaassine <saidoubaassine04@gmail.com>
2024-01-19 16:32:07 +0100

    read one book endpoint

--- api/src/main/java/learn/spring/api/services/BookService.java
+++ api/src/main/java/learn/spring/api/services/BookService.java
@@ -3,9 +3,12 @@ package learn.spring.api.services;
 import learn.spring.api.domain.entities.BookEntity;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookService {
     BookEntity createBook(String isbn, BookEntity book);
 
     List<BookEntity> findAll();
+
+    Optional<BookEntity> findOne(String isbn);
 }
--- api/src/main/java/learn/spring/api/services/impl/BookServiceImpl.java
+++ api/src/main/java/learn/spring/api/services/impl/BookServiceImpl.java
@@ -6,6 +6,7 @@ import learn.spring.api.services.BookService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -29,5 +30,10 @@ public class BookServiceImpl implements BookService {
                 .findAll()
                 .spliterator(), false)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<BookEntity> findOne(String isbn) {
+        return bookRepository.findById(isbn);
     }
 }
